83346b4e726d0e37217c531c52ae73616a2f35d1
Steffan Carter <xisled09@gmail.com>
2022-04-09 11:32:37 -0400

    Button adding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-console.log('test')
-
 var reformatDate = moment().format('dddd, MMMM Do');
 $("#timeEl").text(reformatDate);
+
+var saveButton = document.querySelector("#save-button");
+var sevenEl = document.querySelector("#est");
+
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var userInput = {
+        Hour7: sevenEl.value,
+};
+
+    localStorage.setItem("userInput", JSON.stringify(userInput));
+    renderMessage();
+});
+
+function renderMessage() {
+    var userInput = JSON.parse(localStorage.getItem("userInput"));
+    
+    console.log("userInput")
+}
+  
