eef624b1efe6f4fe4ce0195621ba4fe8b50659f7
Steffan Carter <xisled09@gmail.com>
2022-04-09 23:22:16 -0400

    added colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,40 @@
 var reformatDate = moment().format('dddd, MMMM Do');
 $("#timeEl").text(reformatDate);
 
-// var saveButton = document.querySelector("save-button");
-// var sevenEl = document.querySelector("#seven-hour");
+var saveButton = document.querySelector("save-button");
+var sevenEl = document.querySelector("#msg7");
+var eightEl = document.querySelector("#msg8");
+var nineEl = document.querySelector("#msg9");
+var tenEl = document.querySelector("#msg10");
+var elevenEl = document.querySelector("#msg11");
+var twelveEl = document.querySelector("#msg12");
+var oneEl = document.querySelector("#msg1");
+var twoEl = document.querySelector("#msg2");
+var threeEl = document.querySelector("#msg3");
+var fourEl = document.querySelector("#msg4");
 
-// saveButton.addEventListener("click", function(event) {
-//     event.preventDefault();
-
-//     var userInput = {
-//         Hour7: sevenEl.value.trim(),
-// };
-
-//     localStorage.setItem("userInput", JSON.stringify(userInput));
-//     renderMessage();
-// });
-
-// function renderMessage() {
-//     var userInput = JSON.parse(localStorage.getItem("userInput"));
-    
-//     console.log("userInput")
-// }
-
-var saveButton = document.getElementById("save");
-var comment = document.getElementById("#msg");
-
-saveButton.addEventListener("click", function(event) {
+window.addEventListener("click", function(event) {
     event.preventDefault();
-    
-    var timeSch = {
-      comment: comment.value,
-    };
-    
-    localStorage.setItem("tried", JSON.stringify(timeSch));
-    renderMessage();
-    
-    });
 
-    function renderMessage() {
-        var userInput = JSON.parse(localStorage.getItem("studentGrade"));
-        if (userInput !== null) {
-          document.querySelector(".message").textContent = userInput.student + 
-          " received a/an " + userInput.grade
-        }
-      }
-      
+    var userInput = {
+        Hour7: sevenEl.value,
+        Hour8: eightEl.value,
+        Hour9: nineEl.value,
+        Hour10: tenEl.value,
+        Hour11: elevenEl.value,
+        Hour12: twelveEl.value,
+        Hour1: oneEl.value,
+        Hour2: twoEl.value,
+        Hour3: threeEl.value,
+        Hour4: fourEl.value,
+};
+
+    localStorage.setItem("userInput", JSON.stringify(userInput));
+    renderMessage();
+});
+
+function renderMessage() {
+    var userInput = JSON.parse(localStorage.getItem("userInput"));
+    
+    console.log("userInput")
+}
